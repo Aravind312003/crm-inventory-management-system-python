720ff4765b96e861ce56a5d8703b27fed6a13d6f
Aravind312003 <aravinddeepak03@gmail.com>
2026-05-04 10:16:44 +0530

    updates

--- backend/main.py
+++ backend/main.py
@@ -219,4 +219,4 @@ if os.path.exists(dist_path):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
